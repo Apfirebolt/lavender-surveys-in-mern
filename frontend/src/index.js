631c18a86872aa5e8c58cd726a3f6aa90ba607e9
Apfirebolt <aspper20@gmail.com>
2023-08-26 02:51:30 +0530

    vite removed

---
+++ frontend/src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import store from './store';
+import { Provider } from 'react-redux';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} />
+    </Route>
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <Provider store={store}>
+        <PayPalScriptProvider deferLoading={true}>
+          <RouterProvider router={router} />
+        </PayPalScriptProvider>
+      </Provider>
+    </HelmetProvider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,12 @@
+const HomeScreen = () => {
+
+  
+
+  return (
+    <>
+      <h1>Home Screen</h1>
+    </>
+  );
+};
+
+export default HomeScreen;
